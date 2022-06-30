4a477aef2f9f7518809a4b7721c17840fb70cec7
Float Cat <float.cat@yandex.ru>
2022-06-30 22:53:59 +0700

    Лекция 22 - Статическая библиотека

---
+++ Lecture_22_Static_Library/math_funcs.h
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+int bitOfPos(unsigned int number, short pos);
+int setOfPos(unsigned int number, short pos, int i);
+unsigned int summBin(unsigned int a, unsigned int b, int *flag);
+unsigned int diffBin(unsigned int a, unsigned int b);
+unsigned int multiBin(unsigned int a, unsigned int b);
+unsigned int divBin(unsigned int a, unsigned int b);
+void decToBin(unsigned int d);
