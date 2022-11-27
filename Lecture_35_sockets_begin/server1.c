0f2dff6a828618a654ed0cdada3b4a5a2fce7068
Float Cat <float.cat@yandex.ru>
2022-11-27 15:50:38 +0700

    Мелкие правки

--- Lecture_35_sockets_begin/server1.c
+++ Lecture_35_sockets_begin/server1.c
@@ -13,9 +13,13 @@ int main(void)
     struct sockaddr_un serv, client;
     int fd, fdnew;
     fd = socket(AF_LOCAL, SOCK_STREAM, 0);
+    memset(&serv, 0, sizeof(serv));
     serv.sun_family = AF_LOCAL;
     strcpy(serv.sun_path, "/tmp/connect");
-    bind(fd, (struct sockaddr *)&serv, sizeof(struct sockaddr));
+    if(bind(fd, (struct sockaddr *)&serv, SUN_LEN(&serv)) < 0)
+    {
+        printf("Error!");
+    }
     listen(fd, 5);
     fdnew = accept(fd, 0, 0);
     recv(fdnew, buf, 4, MSG_WAITALL);
